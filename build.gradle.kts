542a291edc439e2000e88bc05b4c83c666d0251c
simon.muia <simonwambua433@gmail.com>
2023-02-21 15:59:00 +0100

    set up ktlint and spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,9 @@
 plugins {
     id("com.android.application") version "8.1.0-alpha04" apply false
     id("com.android.library") version "8.1.0-alpha04" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.google.dagger.hilt.android") version "2.44" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.8.0" apply false
-
+    id("org.jlleitschuh.gradle.ktlint") version "11.0.0" apply false
+    id("com.diffplug.spotless") version "5.15.0" apply false
 }
